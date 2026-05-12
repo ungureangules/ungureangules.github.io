650b5947f4ed59fb89d69f2b7abb5b94efbd085c
Italo Gules <italogules@mpf.mp.br>
2026-05-12 16:19:33 +1000

    fix: remove incorrect working-directory and add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ungureangules.github.io',
   integrations: [react()],
 
   vite: {
